fc8268250941d263720ebcb4c86c9bf58fb812ea
Dmytro Sheremetiev <dmytro.sheremetiev@feelit.studio>
2023-03-03 16:54:40 +0200

    Some changes

--- client/src/api/fetchProfiles.ts
+++ client/src/api/fetchProfiles.ts
@@ -1,6 +1,6 @@
 import { Profile } from "../types/Profile";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = `${process.env.BACKEND_URL}`;
 
 export const getProfiles = (url: string): Promise<Profile[]> => {
   return fetch(`${BASE_URL}/${url}`).then((res) => res.json());
